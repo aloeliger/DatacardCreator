2f77903446c2ae0d8e248edbc435ad680f31aa12
Andrew Loeliger <aloelige@cern.ch>
2020-01-31 11:46:37 -0500

    Initial differential Xsec configuration set-up

---
+++ AnalysisCategories/UserAnalysisCategories/DifferentialCrossSectionCategories/OneJetCategory.py
@@ -0,0 +1,21 @@
+import ROOT
+import AnalysisCategories.AnalysisCategoryDef as AnalysisCategoryDef
+
+def IsInOneJetCategory(theAnalysisCategory,theEventDictionary):
+    if(
+            theEventDictionary.eventDictionary['TauPt'] >= 30.0
+            and theEventDictionary.eventDictionary['MT'] < 50.0
+            and theEventDictionary.eventDictionary['Njets'] == 1
+            ):
+        return True
+    else:
+        return False
+    return False
+
+OneJet = AnalysisCategoryDef.AnalysisCategory()
+OneJet.name = "mt_1jet"
+OneJet.IsInCategory = IsInOneJetCategory
+OneJet.rollingVariable = 'HiggsPt'
+OneJet.rollingBins = [0,20,45,80,120,200,350,10000]
+OneJet.reconstructionVariable= 'M_sv'
+OneJet.reconstructionBins = [50.0,70.0,90.0,110.0,130.0,150.0,170.0,210.0,250.0,9000.0]
---
+++ AnalysisCategories/UserAnalysisCategories/DifferentialCrossSectionCategories/TwoJetLowMjjCategory.py
@@ -0,0 +1,22 @@
+import ROOT
+import AnalysisCategories.AnalysisCategoryDef as AnalysisCategoryDef
+
+def IsInTwoJetLowCategory(theAnalysisCategory,theEventDictionary):
+    if(
+            theEventDictionary.eventDictionary['TauPt'] >= 30.0
+            and theEventDictionary.eventDictionary['MT'] < 50.0
+            and theEventDictionary.eventDictionary['Njets'] == 2
+            and theEventDictionary.eventDictioanry['mjj'] < 450.0
+            ):
+        return True
+    else:
+        return False
+    return False
+
+TwoJetLow = AnalysisCategoryDef.AnalysisCategory()
+TwoJetLow.name = "mt_2jet_low"
+TwoJetLow.IsInCategory = IsInTwoJetLowCategory
+TwoJetLow.rollingVariable = 'HiggsPt'
+TwoJetLow.rollingBins = [0,20,45,80,120,200,350,10000]
+TwoJetLow.reconstructionVariable= 'M_sv'
+TwoJetLow.reconstructionBins = [50.0,70.0,90.0,110.0,130.0,150.0,170.0,210.0,250.0,9000.0]
