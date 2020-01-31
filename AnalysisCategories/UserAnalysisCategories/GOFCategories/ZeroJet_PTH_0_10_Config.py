084fc4c9f675708c70e051e3d4b1682426f953b6
Andrew Loeliger <aloelige@cern.ch>
2020-01-31 11:25:42 -0500

    Added in specially blinded GOF categories

---
+++ AnalysisCategories/UserAnalysisCategories/GOFCategories/ZeroJet_PTH_0_10_Config.py
@@ -0,0 +1,23 @@
+import ROOT
+import AnalysisCategories.AnalysisCategoryDef as AnalysisCategoryDef
+
+def IsInZeroJetLowCategory(theAnalysisCategory,theEventDictionary):
+    if(
+            theEventDictionary.eventDictionary['TauPt'] >= 30.0
+            and theEventDictionary.eventDictionary['MT'] < 50.0
+            and theEventDictionary.eventDictionary['Njets'] == 0
+            and theEventDictionary.eventDictionary['DeltaR'] > 3.0
+            and (theEventDictionary.eventDictionary['M_sv'] <= 90.0 or theEventDictionary.eventDictionary['M_sv'] >= 150.0)
+    ):
+        return True
+    else:
+        return False
+    return False
+    
+ZeroJetLow = AnalysisCategoryDef.AnalysisCategory()
+ZeroJetLow.name = 'mt_0jet_PTH_0_10'
+ZeroJetLow.IsInCategory = IsInZeroJetLowCategory
+ZeroJetLow.rollingVariable  = 'TauPt'
+ZeroJetLow.rollingBins = [30.0,40.0,50.0,10000.0]
+ZeroJetLow.reconstructionVariable='M_sv'
+ZeroJetLow.reconstructionBins=[50.0,70.0,90.0,110.0,130.0,150.0,170.0,210.0,250.0,9000.0]
