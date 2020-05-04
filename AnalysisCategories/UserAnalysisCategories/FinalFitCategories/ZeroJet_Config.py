1172438d81f095186b193639247042072c505437
Andrew Loeliger <aloelige@cern.ch>
2020-05-04 07:03:53 -0400

    Add forgotten single zero jet category for STXS

---
+++ AnalysisCategories/UserAnalysisCategories/FinalFitCategories/ZeroJet_Config.py
@@ -0,0 +1,22 @@
+import ROOT
+import AnalysisCategories.AnalysisCategoryDef as AnalysisCategoryDef
+
+def IsInZeroJetCategory(theAnalysisCategory,theEventDictionary):
+    if (
+            theEventDictionary.eventDictionary['TauPt'] >= 30.0
+            and theEventDictionary.eventDictionary['MT'] < 50.0
+            and theEventDictionary.eventDictionary['Njets'] == 0            
+            and theEventDictionary.eventDictionary['DeltaR'] > 2.0
+    ):        
+        return True
+    else:
+        return False
+    return False
+
+ZeroJet = AnalysisCategoryDef.AnalysisCategory()
+ZeroJet.name = 'mt_0jet'
+ZeroJet.IsInCategory = IsInZeroJetCategory
+ZeroJet.rollingVariable = 'TauPt'
+ZeroJet.rollingBins = [30.0,40.0,50.0,10000.0]
+ZeroJet.reconstructionVariable = 'M_sv'
+ZeroJet.reconstructionBins = [50.0,70.0,90.0,110.0,130.0,150.0,170.0,210.0,250.0,290.0]
